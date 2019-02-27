fb8081847a0f8e5c73193d2ef87e0f19878beedd
Eduardo Sotero <eduardo@feracode.com>
2019-02-27 18:14:29 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,54 @@ var passport = require("passport");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Express" });
+  res.render("index", { title: "Express" });
 });
 
 /* GET Google Authentication API. */
 router.get(
-	"/auth/google",
-	passport.authenticate("google", { scope: ["profile", "email"] })
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 router.get(
-	"/auth/google/callback",
-	passport.authenticate("google", { failureRedirect: "/", session: false }),
-	function(req, res) {
-		var token = req.user.token;
-		res.redirect("http://localhost:3000?token=" + token);
-	}
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/", session: false }),
+  function(req, res) {
+    var token = req.user.token;
+    res.redirect("http://localhost:3000?token=" + token);
+  }
+);
+
+/* GET Facebook Authentication API. */
+router.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+
+router.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: "/"
+  }),
+  function(req, res) {
+    var token = req.user.token;
+    res.redirect("http://localhost:3000?token=" + token);
+  }
+);
+
+/* GET Twitch Authentication API. */
+router.get(
+  "/auth/twitch",
+  passport.authenticate("twitch", { forceVerify: true })
+);
+
+router.get(
+  "/auth/twitch/callback",
+  passport.authenticate("twitch", { failureRedirect: "/", session: false }),
+  function(req, res) {
+    var token = req.user.token;
+    res.redirect("http://localhost:3000?token=" + token);
+  }
 );
 
 module.exports = router;
